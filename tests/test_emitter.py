d7aa8294051f82618058df3ade9b860072eb0443
Abe Gong <abegong@gmail.com>
2024-08-05 00:13:08 -0600

    * Add tests for the `IntervalEmitter.skip_probability` attribute * Clean up test_emitter.py

--- tests/test_emitter.py
+++ tests/test_emitter.py
@@ -1,14 +1,11 @@
-from dgpinata import Event
-from dgpinata import Emitter, IntervalEmitter
-from dgpinata.action import AddEvent
+import random
+
+from dgpinata import IntervalEmitter
 
 def _extract_event_timestamps(events):
     return [event.timestamp for event in events]
 
 def test__interval_emitter__from_params():
-    # class MyEvent(Event):
-    #     foo: int
-    #     bar: int
 
     emitter = IntervalEmitter.from_params(
         event_type_name="MyEvent",
@@ -39,19 +36,26 @@ def test__interval_emitter__interval_ranges():
         timestamp=30
     )) == 1
 
-    # If a full interval has elapsed, an event is emitted
+    # If exactly one full interval has elapsed, then exactly one event is emitted
     assert len(emitter.emit(
         parent=None,
         prev_timestamp=0,
         timestamp=60
     )) == 1
 
+    # If more than one full interval has elapsed, then two events are emitted
+    assert len(emitter.emit(
+        parent=None,
+        prev_timestamp=0,
+        timestamp=61
+    )) == 2
+
     # If multiple intervals have elapsed, multiple events are emitted
     assert len(emitter.emit(
         parent=None,
         prev_timestamp=0,
-        timestamp=120
-    )) == 2
+        timestamp=200
+    )) == 4
 
     # If multiple intervals have elapsed, multiple events are emitted
     assert len(emitter.emit(
@@ -60,77 +64,48 @@ def test__interval_emitter__interval_ranges():
         timestamp=3600
     )) == 60
 
-    # Intervals are inclusive
+    # Intervals are inclusive of `prev_timestamp` and exclusive of `timestamp`
     assert len(emitter.emit(
         parent=None,
         prev_timestamp=60,
         timestamp=120
     )) == 1
 
-    # assert emitter.emit(
-    #     parent=None,
-    #     prev_timestamp=0,
-    #     timestamp=0
-    # )[0] == AddEvent(
-    #     event_type_name="SomeEvent",
-    #     parameter_builders={},
-    #     parent=None,
-    #     timestamp=0,
-    # )
-    # print(emitter.emit(parent=None, timestamp=1))
-    # print(emitter.emit(parent=None, timestamp=3600))
-
-    # assert False
-
-
-    # If a full interval has elapsed, an event is emitted
-    # If multiple intervals have elapsed, multiple events are emitted
-
 def test__interval_emitter__skip_probability():
+    random.seed(1)
+    
     # If `skip_probability` is 0, all events are emitted
+    emitter = IntervalEmitter.from_params(
+        event_type_name="SomeEvent",
+        interval=60,
+        skip_probability=0.0,
+    )
+    assert len(emitter.emit(
+        parent=None,
+        prev_timestamp=0,
+        timestamp=3600
+    )) == 60
+
     # If `skip_probability` is 1, no events are emitted
+    emitter = IntervalEmitter.from_params(
+        event_type_name="SomeEvent",
+        interval=60,
+        skip_probability=1.0,
+    )
+    assert len(emitter.emit(
+        parent=None,
+        prev_timestamp=0,
+        timestamp=3600
+    )) == 0
+
     # If `skip_probability` is 0.5, half of the events are emitted
-    pass
-
-    # Space events by `spacing`
-
-    # Event timestamps should be offset by `offset`
-    # `offset` can be a fixed value or a callable
-
-
-
-# "add_customer": IntervalEmitter(
-#     event_type=Customer,
-#     interval=3600,
-#     offset=0,
-#     skip_probability=0.8,
-#     created_at="timestamp",
-# ),
-# "add_sale": IntervalEmitter(
-#     event_type=Sale,
-#     interval=3600,
-#     -- offset=lambda: random.randint(),
-#     product_id=RandomChoice("sim.Entities['Product']"),
-#     customer_id=RandomChoice("sim.Entities['Customer']"),
-#     amount=1,
-#     timestamp="timestamp",
-# ),
-# "add_customer_with_new_sale": IntervalEmitter(
-#     interval=3600,
-#     offset=0,
-#     events=[
-#         EventGenerator(
-#             event_type=Customer,
-#             var_name="new_customer",
-#             skip_probability=0.8,
-#             created_at="timestamp",
-#         ),
-#         EventGenerator(
-#             event_type=Sale,
-#             product_id=RandomChoice("sim.Entities['Product']"),
-#             customer_id=SyntaxChooser("new_customer.customer_id"),
-#             amount=1,
-#             timestamp="timestamp",
-#         ),
-#     ]
-# ),
+    emitter = IntervalEmitter.from_params(
+        event_type_name="SomeEvent",
+        interval=60,
+        skip_probability=0.5,
+    )
+    assert len(emitter.emit(
+        parent=None,
+        prev_timestamp=0,
+        timestamp=3600
+    )) == 37
